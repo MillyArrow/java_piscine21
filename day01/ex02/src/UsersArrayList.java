7f90b69cc87923005e8c610d0ad7b6c489bbbe2b
dfg13 <55473206+dfg13@users.noreply.github.com>
2021-02-03 18:31:36 +0300

    Update UsersArrayList.java
    ex02 day 01

--- day01/ex02/src/UsersArrayList.java
+++ day01/ex02/src/UsersArrayList.java
@@ -1,2 +1,31 @@
 public class UsersArrayList {
+	User[] users = new User[10];
+	public void addUser(User user){
+		for(int i = 0; i < users.length; i++){
+			if (users[i] == null){
+				users[i] = user;
+				break;
+			}
+			if (i == users.length - 1){
+				User[] new_users = new User[users.length + users.length / 2];
+				for(int j = 0; j < users.length; j++){
+					new_users[j] = users[j];
+				}
+				users = new_users;
+			}
+		}
+	}
+	User getUserByID(){
+
+	}
+	User getUserByIndex(){
+
+	}
+	public Integer numberOfUsers(){
+		int i = 0;
+		while (users[i] != null){
+			++i;
+		}
+		return i;
+	}
 }
